f670fdf3ed537971cb2f4a9fb07a6fc15cb5d145
NMTIEN05 <nmtbao001@gmail.com>
2025-07-06 18:20:23 +0700

    update order

--- src/model/Order.js
+++ src/model/Order.js
@@ -4,11 +4,21 @@ const orderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "UserModel", required: true },
   items: [{ type: mongoose.Schema.Types.ObjectId, ref: "OrderItem" }],
   totalAmount: { type: Number, required: true },
-  status: {
-    type: String,
-    enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
-    default: "pending",
-  },
+status: {
+  type: String,
+  enum: [
+    "pending",             // Chờ xác nhận
+    "processing",          // Đang xử lý
+    "ready_to_ship",       // Chờ giao hàng
+    "shipped",             // Đang giao
+    "delivered",           // Đã giao
+    "return_requested",    // Yêu cầu trả hàng ← thêm
+    "returned",            // Đã hoàn trả
+    "cancelled"            // Đã hủy
+  ],
+  default: "pending"
+},
+
   shippingAddress: { type: String, required: true },
   paymentMethod: {
   type: String,
