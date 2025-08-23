70e48d82ce30f6624d5b2c86ad9ba38c248fa891
NMTIEN05 <nmtbao001@gmail.com>
2025-08-23 22:28:36 +0700

    updateb checkout

--- src/model/flashSale.js
+++ src/model/flashSale.js
@@ -2,18 +2,18 @@ import mongoose from "mongoose";
 
 const flashSaleSchema = new mongoose.Schema({
   product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-   variant: { type: mongoose.Schema.Types.ObjectId, ref: "Variant", required: true }, 
+  variant: { type: mongoose.Schema.Types.ObjectId, ref: "Variant", required: true },
   salePrice: { type: Number, required: true },
-  quantity: { type: Number, required: true },
+  quantity: { type: Number, required: true }, // tổng số xuất flash sale
+  soldQuantity: { type: Number, default: 0 }, // số lượng đã bán
   discountPercent: { type: Number, required: true },
   startTime: { type: Date, required: true },
   endTime: { type: Date, required: true },
-  limitQuantity: { type: Number, default: 0 },
+  limitQuantity: { type: Number, default: 0 }, // mỗi khách được mua tối đa
   isActive: { type: Boolean, default: true },
 }, { timestamps: true });
 
-// Đảm bảo **mỗi flash sale chỉ sale 1 sản phẩm tại 1 thời điểm**
-// Nếu muốn cùng 1 sản phẩm có nhiều flash sale khác thời gian, bỏ index unique
+// Đảm bảo mỗi flash sale chỉ sale 1 sản phẩm tại 1 thời điểm
 // flashSaleSchema.index({ product: 1 }, { unique: true });
 
 export default mongoose.model("FlashSale", flashSaleSchema);
